d904aa726e17c7e45930ce140ccc1f6cd08e8260
Todor Chakarov <todorchakarov@Todors-MacBook-Pro.local>
2018-05-26 17:26:24 +0300

    Fix upload pic to project

--- app/Http/Helpers/ImagesHelper.php
+++ app/Http/Helpers/ImagesHelper.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Order;
-use App\Admin\Category;
-use App\Admin\SubCategory;
 use App\Admin\Product;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 use Image;
 
@@ -44,18 +40,34 @@ class ImagesHelper{
     public static function resizeImages($file_main_pic, $productId, $water_checked, $width, $height)
     {
         $extension = $file_main_pic->getClientOriginalExtension();
-
         $fileNameToStore = 'basic_'.time().'.'.$extension;
 
-        Storage::makeDirectory('public/upload_pictures/'.$productId);
+        $rootProjectPatchDir = base_path();
+        $outsideProjectPatchDir = dirname($rootProjectPatchDir, 1);
 
-        //$result = File::makeDirectory('/path/to/directory');
+        if(!is_dir($outsideProjectPatchDir.'/shared/storage/upload_pictures')){
+            File::makeDirectory($outsideProjectPatchDir.'/shared');
+            File::makeDirectory($outsideProjectPatchDir.'/shared/storage');
+            File::makeDirectory($outsideProjectPatchDir.'/shared/storage/upload_pictures');
+        }
+
+        if(!is_dir($outsideProjectPatchDir.'/shared/storage/common_pictures')) {
+            File::makeDirectory($outsideProjectPatchDir . '/shared/storage/common_pictures');
+        }
+
+        $upload_pic_path = $outsideProjectPatchDir.'/shared/storage/upload_pictures';
+        $common_pic_path = $outsideProjectPatchDir . '/shared/storage/common_pictures';
+
+        if(!is_dir($upload_pic_path.'/'.$productId)) {
+            File::makeDirectory($upload_pic_path.'/'.$productId);
+        }
 
         $image = Image::make($file_main_pic->getRealPath());
+        $path = $upload_pic_path.'/'.$productId.'/'. $fileNameToStore;
+        $water_mark = $common_pic_path.'/watermark.png';
 
-        $path = storage_path('app/public/upload_pictures/'.$productId.'/'. $fileNameToStore);
 
-        $water_mark = storage_path('app/public/common_pictures/watermark.png');
+        echo var_dump($path);
 
         if(file_exists($water_mark) && $water_checked == 1)
         {

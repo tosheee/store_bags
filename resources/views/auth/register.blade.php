4382f2f72aa10c4f20c57acb2d5daa10bbbd430e
Todor Chakarov <tosheee@abv.bg>
2018-07-13 11:38:27 +0300

    Change style for auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,95 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="wrapper-login-form fadeInDown">
+        <div id="formContent">
+            <!-- Tabs Titles -->
+            <h2 class="active"> Регистрация </h2>
 
-    <br/><br/>
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Регистрация</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Име</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Е-мейл</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Парола</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Повтори парола</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="confirm_condition" class="col-md-4 control-label"></label>
-                            <div class="col-md-6">
-                                <input id="confirm_condition" type="checkbox" class="" name="" required="required" oninvalid="this.setCustomValidity('За да продължите, моля кликнете върху отметката')" oninput="setCustomValidity('')">
-                                Ние имаме нужда от Вашето съгласие, за да обработваме личните Ви данни в съответствие с новия Регламент! Моля прочетете нашата
-                                <a href="/personal_data">Политика за защита на лични данни!</a>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="use_condition" class="col-md-4 control-label"></label>
-                            <div class="col-md-6">
-                                <input id="use_condition" type="checkbox" class="" name="" required="required" oninvalid="this.setCustomValidity('За да продължите, моля кликнете върху отметката')" oninput="setCustomValidity('')">
-                                <a href="/terms_of_use">Приемам условията за ползване</a>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Регистрация
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <!-- Icon -->
+            <div class="fadeIn first">
+                <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
             </div>
+
+            <!-- Login Form -->
+            <form class="form-group" method="POST" action="{{ route('register') }}">
+                {{ csrf_field() }}
+                <input type="text" id="name" class="fadeIn second" name="name" placeholder="Име"  value="{{ old('name') }}" required autofocus>
+                @if ($errors->has('name'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('name') }}</strong>
+                    </span>
+                @endif
+
+                <input type="email" id="email" class="fadeIn second" name="email" placeholder="Е-мейл" value="{{ old('email') }}" required>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+
+
+                <input type="password" id="password" class="fadeIn second" name="password" placeholder="Парола" required>
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+
+                <input type="password" id="password-confirm" class="fadeIn third" name="password_confirmation" placeholder="Повтори Парола" required>
+                @if ($errors->has('password_confirmation'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                    </span>
+                @endif
+
+                <div class="form-group">
+                    <input id="confirm_condition" type="checkbox" class="" name="" required="required" oninvalid="this.setCustomValidity('За да продължите, моля кликнете върху отметката')" oninput="setCustomValidity('')">
+                    Ние имаме нужда от Вашето съгласие, за да обработваме личните Ви данни в съответствие с новия Регламент! Моля прочетете нашата
+                    <br/>
+                    <a href="/personal_data">Политика за защита на лични данни!</a>
+                </div>
+
+                <div class="form-group">
+                    <input id="use_condition" type="checkbox" class="" name="" required="required" oninvalid="this.setCustomValidity('За да продължите, моля кликнете върху отметката')" oninput="setCustomValidity('')">
+                    <a href="/terms_of_use">Приемам условията за ползване</a>
+                </div>
+
+                <input type="submit" class="fadeIn fourth" value="Регистрация">
+            </form>
         </div>
     </div>
-</div>
 @endsection

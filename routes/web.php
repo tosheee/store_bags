c0a1a8445add684b5d816024f96b2361dabcd099
Todor Chakarov <tosheee@abv.bg>
2018-07-15 10:49:10 +0300

    Refactor routes

--- routes/web.php
+++ routes/web.php
@@ -1,137 +1,71 @@
 <?php
+    View::composer('*', function($view) { $view->with('siteViewInformation', App\Admin\InfoCompany::orderBy('created_at', 'desc')->first()); });
+    View::composer('*', function($view) { $view->with('categoriesButtonsName', App\Admin\Category::all()); });
+    View::composer('*', function($view) { $view->with('subCategoriesButtonsName', App\Admin\SubCategory::all()); });
+    View::composer('*', function($view) {$view->with('subCategories', App\Admin\SubCategory::all());});
+    View::composer('*', function($view) {$view->with('allSliderData', App\Admin\Slider::all());});
+    View::composer('*', function($view) {
+        $view->with('pagesButtonsRender', App\Admin\Page::where('active_page', true)->get());
+    });
 
-Route::get('/', 'StoreController@index');
+    Auth::routes();
 
-View::composer('*', function($view) { $view->with('siteViewInformation', App\Admin\InfoCompany::orderBy('created_at', 'desc')->first()); });
+    //Route::get('/', 'WelcomeController@index');
 
-View::composer('*', function($view) { $view->with('categoriesButtonsName', App\Admin\Category::all()); });
+    Route::get('/', 'StoreController@index');
 
-View::composer('*', function($view) { $view->with('subCategoriesButtonsName', App\Admin\SubCategory::all()); });
+    Route::get('/store',                       ['uses' => 'StoreController@index',          'as'   => 'store.index']);
+    Route::get('/store/view_user_orders/{id}', ['uses' => 'StoreController@viewUserOrders', 'as'   => 'store.index']);
+    Route::get('/store/search',                ['uses' => 'SearchController@search',        'as'   => 'store.search']);
+    Route::get('/store/{id}',                  ['uses' => 'StoreController@show',           'as'   => 'store.show']);
+    Route::post('/store/like_product/{id}',    'StoreController@getLikeProduct');
 
-View::composer('*', function($view) {$view->with('subCategories', App\Admin\SubCategory::all());});
-
-View::composer('*', function($view) {$view->with('allSliderData', App\Admin\Slider::all());});
-
-View::composer('*', function($view) {
-    $view->with('pagesButtonsRender', App\Admin\Page::where('active_page', true)->get());
-});
-
-Auth::routes();
-
-Route::get('/page', [
-    'uses' => 'StoreController@getShowPages',
-    'as'  => 'store.showPage'
-]);
-
-Route::get('/store', [
-    'uses' => 'StoreController@index',
-    'as'   => 'store.index'
-]);
-
-Route::get('/store/view_user_orders/{id}', [
-    'uses' => 'StoreController@viewUserOrders',
-    'as'   => 'store.index'
-]);
-
-Route::get('/store/search', [
-    'uses' => 'SearchController@search',
-    'as'   => 'store.search'
-]);
-
-Route::get('/store/{id}', [
-    'uses' => 'StoreController@show',
-    'as'   => 'store.show'
-]);
-
-Route::post('admin/products/create/{id?}', function($id = null) {
-
-    $subCategoryAttributes = App\Admin\SubCategory::where('category_id', $id)->get();
-    $subCategoryOptions = array();
-    foreach($subCategoryAttributes as $key => $subCatAttribute)
-    {
-        $subCategoryOptions[$key] = [$subCatAttribute->id, $subCatAttribute->name, $subCatAttribute->identifier];
-    }
-
-    return $subCategoryOptions;
-});
+    Route::get('/page',               ['uses' => 'StoreController@getShowPages',    'as'  => 'store.showPage']);
+    Route::post('/add-to-cart',       'StoreController@getAddToCart');
+    Route::post('/send-user-message', 'StoreController@postUserMessage');
+    Route::post('/remove/{id}',       ['uses' => 'StoreController@getRemoveItem',   'as'  => 'store.remove']);
+    Route::get('/checkout',           ['uses' => 'StoreController@getCheckout',     'as'  => 'store.checkout']);
+    Route::get('/terms_of_use',       ['uses' => 'StoreController@getTermsOfUse',   'as'  => 'store.terms_of_use']);
+    Route::get('/personal_data',      ['uses' => 'StoreController@getPersonalData', 'as'  => 'store.personal_data']);
+    Route::get('/shopping-cart',      ['uses' => 'StoreController@getCart',         'as'  => 'store.shoppingCart']);
+    Route::post('/checkout',          'StoreController@postCheckout');
 
 
-Route::post('/add-to-cart', 'StoreController@getAddToCart');
+    // Admin
+    Route::group(['prefix' => 'admin'], function() {
+        Route::get    ('/dashboard',            ['uses' => 'AdminController@index']);
+        Route::get    ('/not_completed_orders', ['uses' => 'AdminController@not_completed_orders']);
+        Route::get    ('/dashboard/{id}',       ['uses' => 'AdminController@viewOffer']);
+        Route::get    ('/completed_order/{id}', ['uses' => 'AdminController@completedOrder']);
+        Route::delete ('/dashboard/{id}',       ['uses' => 'AdminController@destroy']);
+    });
 
-Route::post('/send-user-message', 'StoreController@postUserMessage');
-
-Route::post('/store/like_product/{id}', 'StoreController@getLikeProduct');
-
-Route::post('/remove/{id}', [
-    'uses' => 'StoreController@getRemoveItem',
-    'as'  => 'store.remove'
-]);
-
-Route::get('/checkout', [
-    'uses' => 'StoreController@getCheckout',
-    'as'  => 'store.checkout'
-]);
-
-
-Route::get('/terms_of_use', [
-    'uses' => 'StoreController@getTermsOfUse',
-    'as'  => 'store.terms_of_use'
-]);
+    Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function() {
+        Route::get('/products/search', ['uses' => 'ProductsController@search_category', 'as' => 'search_category' ]);
+        Route::get ('',                ['uses' => 'LoginController@showLoginForm',      'as' => 'admin.login']);
+        Route::post('',                ['uses' => 'LoginController@login']);
+        Route::get ('/answer/{id}',    ['uses' => 'UserMessagesController@markAnswer']);
 
 
-Route::get('/personal_data', [
-    'uses' => 'StoreController@getPersonalData',
-    'as'  => 'store.personal_data'
-]);
+        Route::resource('/categories',       'CategoriesController');
+        Route::resource('/sub_categories',   'SubCategoriesController');
+        Route::resource('/products',         'ProductsController');
+        Route::resource('/users',            'UserController');
+        Route::resource('info_company',      'InfoCompanyController');
+        Route::resource('admins',            'AdminController');
+        Route::resource('/pages',            'PagesController');
+        Route::resource('/slider',           'SliderController');
+        Route::resource('/user_messages',    'UserMessagesController');
+        Route::resource('/support_messages', 'SupportMessagesController');
+    });
 
+    Route::post('admin/products/create/{id?}', function($id = null) {
+        $subCategoryAttributes = App\Admin\SubCategory::where('category_id', $id)->get();
+        $subCategoryOptions = array();
+        foreach($subCategoryAttributes as $key => $subCatAttribute)
+        {
+            $subCategoryOptions[$key] = [$subCatAttribute->id, $subCatAttribute->name, $subCatAttribute->identifier];
+        }
 
-
-Route::post('/checkout', 'StoreController@postCheckout');
-
-Route::get('/shopping-cart', [
-    'uses' => 'StoreController@getCart',
-    'as'  => 'store.shoppingCart'
-]);
-
-// Admin
-
-Route::get('/admin/products/search', [
-    'uses' => 'Admin\ProductsController@search_category',
-    'as'   => 'search_category'
-]);
-
-Route::get ('/admin/dashboard', 'AdminController@index');
-
-Route::get ('/admin/not_completed_orders', 'AdminController@not_completed_orders');
-
-Route::get ('/admin/dashboard/{id}', 'AdminController@viewOffer');
-
-Route::get ('/admin/completed_order/{id}', 'AdminController@completedOrder');
-
-Route::delete ('/admin/dashboard/{id}', 'AdminController@destroy');
-
-Route::get ('/admin', 'Admin\LoginController@showLoginForm')->name('admin.login');
-
-Route::post('/admin', 'Admin\LoginController@login');
-
-Route::resource('/admin/categories', 'Admin\CategoriesController');
-
-Route::resource('/admin/sub_categories', 'Admin\SubCategoriesController');
-
-Route::resource('/admin/products', 'Admin\ProductsController');
-
-Route::resource('/admin/users', 'Admin\UserController');
-
-Route::resource('/admin/info_company', 'Admin\InfoCompanyController');
-
-Route::resource('/admin/admins', 'Admin\AdminController');
-
-Route::resource('/admin/pages', 'Admin\PagesController');
-
-Route::resource('/admin/slider', 'Admin\SliderController');
-
-Route::get ('/admin/answer/{id}', 'Admin\UserMessagesController@markAnswer');
-
-Route::resource('/admin/user_messages', 'Admin\UserMessagesController');
-
-Route::resource('/admin/support_messages', 'Admin\SupportMessagesController');
+        return $subCategoryOptions;
+    });

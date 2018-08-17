325103a106bdf247a0039bce34e1e2c56aaeaf2d
Todor Chakarov <tosheee@abv.bg>
2018-08-17 12:19:15 +0300

    Fix boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        URL::forceScheme('https');
+        if ($_SERVER['HTTP_HOST'] == 'localhost:8000'){
+            URL::forceScheme('http');
+        }else{
+            URL::forceScheme('https');
+        }
     }
 
     /**
